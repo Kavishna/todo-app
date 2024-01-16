a93f18a22b7503ae9dd07c0a0221c2f3f40874c0
Kavishna Thathsara <kavishna@Kavishnas-MacBook-Pro.local>
2024-01-16 19:56:58 +0530

    day2 finished working on mynotes component

--- frontend/src/components/mynotes/Mynotes.jsx
+++ frontend/src/components/mynotes/Mynotes.jsx
@@ -3,28 +3,51 @@ import "./Mynotes.css";
 
 const Mynotes = () => {
   return (
-    <div className="notes-container">
-      <div className="note">
-        <div className="title">
-          <h4>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas,
-            inventore.
-          </h4>
+    <div className="outer-container">
+      <div className="notes-container">
+        <h2>
+          Welcome <span>Kavishna</span>
+        </h2>
+        <div className="note">
+          <div className="title">
+            <h4>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Voluptas, inventore.
+            </h4>
+          </div>
+          <div className="content">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+              natus ut aliquid quasi asperiores voluptate assumenda distinctio
+              rem dolores fugiat?
+            </p>
+          </div>
+          <div className="date">
+            <p>2023/12/03 13:01</p>
+          </div>
         </div>
-        <div className="content">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor natus
-            ut aliquid quasi asperiores voluptate assumenda distinctio rem
-            dolores fugiat?
-          </p>
+        <div className="note">
+          <div className="title">
+            <h4>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Voluptas, inventore.
+            </h4>
+          </div>
+          <div className="content">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+              natus ut aliquid quasi asperiores voluptate assumenda distinctio
+              rem dolores fugiat?
+            </p>
+          </div>
+          <div className="date">
+            <p>2023/12/03 13:01</p>
+          </div>
         </div>
-        <div className="date">
-          <p>2023/12/03 13:01</p>
+        <div className="new-note">
+          <img src={addIcon} alt="add a new note" />
+          <p>Add New Note</p>
         </div>
-      </div>
-      <div className="new-note">
-        <img src={addIcon} alt="add a new note" />
-        <p>Add New Note</p>
       </div>
     </div>
   );
