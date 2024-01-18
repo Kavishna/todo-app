63e0c3b58ed1edb2024739802c1d140e2866d7bf
Kavishna Thathsara <kavishna@Kavishnas-MacBook-Pro.local>
2024-01-18 22:26:43 +0530

    day4 - newnote component is done

--- frontend/src/components/newnote/Newnote.jsx
+++ frontend/src/components/newnote/Newnote.jsx
@@ -5,29 +5,31 @@ import addIcon from "../../assets/add.svg";
 const Newnote = () => {
   return (
     <div className="add">
-      <div className="container">
-        <h2>Add New Note</h2>
-        <input type="text" className="title" placeholder="Title" />
-        <textarea
-          cols="30"
-          rows="10"
-          placeholder="Start typing..."
-          className="description"
-        ></textarea>
-        <div className="actions">
-          <div className="colors">
-            <button className="color"></button>
-            <button className="color selected"></button>
-            <button className="color"></button>
-            <button className="color"></button>
-          </div>
-          <div className="opts">
-            <button className="delete">
-              <img src={deleteIcon} alt="delete this note" />
-            </button>
-            <button className="add">
-              <img src={addIcon} alt="add a note" />
-            </button>
+      <div className="center">
+        <div className="container">
+          <h2>Add New Note</h2>
+          <input type="text" className="title" placeholder="Title" />
+          <textarea
+            cols="30"
+            rows="10"
+            placeholder="Start typing..."
+            className="description"
+          ></textarea>
+          <div className="actions">
+            <div className="colors">
+              <button className="pink color"></button>
+              <button className=" yellow color selected"></button>
+              <button className="green color"></button>
+              <button className="blue color"></button>
+            </div>
+            <div className="opts">
+              <button className="delete">
+                <img src={deleteIcon} alt="delete this note" />
+              </button>
+              <button className="add">
+                <img src={addIcon} alt="add a note" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
